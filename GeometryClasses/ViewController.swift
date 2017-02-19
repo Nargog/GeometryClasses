fac5eb932ad96423bb0d5e902777b45717df69c7
Mats Hammarqvist <Mats@Matss-MacBook-Pro.local>
2017-02-19 22:44:54 +0100

    Initial Commit

---
+++ GeometryClasses/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GeometryClasses
+//
+//  Created by Mats Hammarqvist on 2017-02-19.
+//  Copyright © 2017 Mats Hammarqvist. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
